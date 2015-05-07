88be651c7108ef70534eae348177810227aa1fbd
Albin George <albinin0002@gmail.com>
2015-05-07 12:52:04 +0530

    Check if all products are inserted into mongo

--- src/main/java/com/unbxd/GetProductsExecutor.java
+++ src/main/java/com/unbxd/GetProductsExecutor.java
@@ -3,10 +3,7 @@ package com.unbxd;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 /**
  * Created by albin on 4/30/15.
@@ -42,22 +39,23 @@ public class GetProductsExecutor {
             this.list.add(submit);
         }
         int i = 0;
-//        for (Future<Boolean> future : list) {
-//            try {
-//                if(future.get()) {
-//                    System.out.println("Products inserted for page: " + i);
+        for (Future<Boolean> future : list) {
+            try {
+                if (future.get()) {
+                    System.out.println("Products inserted for page: " + i);
 
-//                } else {
-//                    System.out.println("ERROR : Products not inserted for page: " + i);
-//                }
-//
-//                i++;
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            } catch (ExecutionException e) {
-//                e.printStackTrace();
-//            }
-//        }
+                } else {
+                    System.out.println("ERROR : Products not inserted for page: " + i);
+                }
+
+                i++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
         executor.shutdown();
     }
 }
